7995c37c0465b3045d963ebea9039fa8f93edf15
Christian Schmid <Christian.Schmid1@HS-Augsburg.DE>
2019-10-08 18:17:37 +0200

    Feedback von Canvas gepushed

---
+++ Informatik_2/Eigene_Projekte/Canvas- Background/Feedback/background.h
@@ -0,0 +1,52 @@
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+#include <iostream>
+
+// CM: #include <string> (fuer string) ergänzen
+
+using namespace std; // NAMESPACE IN DEN HEADER ODER INS CPP FILE? // CM: CPP
+
+class Background
+{
+private:
+
+    string b_color;
+    unsigned int size_x;
+    unsigned int size_y;
+    unsigned int arraylength;
+
+
+    uint8_t r; // CM: <- Was sollen diese drei Werte?
+    uint8_t g;
+    uint8_t b;
+    uint8_t pixelval[200*300*3]; // CM: Falls variable Größe des Bildes verwendet werden soll, steht hier nur die Startadresse des Feldes: uint8_t *pixelval;
+    // CM: Sie könnten auch mit DEFINES für die Zahlen arbeiten :-)
+    void setcolor();
+    void paint();
+
+
+public:
+
+
+    Background(std::string color); // CM: Das "std::" ist nur nötig, wenn kein "using namespace std;" oben steht. Da das eher in die cpp soll, bitte lassen :-)
+    Background();
+    ~Background();
+
+    uint8_t * getarray() // CM: unnötig, wenn save-Methdoen in der Klasse sind. So öffnen Sie durch Rückgabe der Startadresse des Feldes unkontrolliert den Zugriff auf den Bildinhalt.
+    {
+        return this->pixelval;
+    }
+    unsigned int getSize_x()
+    {
+        return this->size_x;
+    }
+    unsigned int getSize_y()
+    {
+        return this->size_y;
+    }
+
+
+};
+
+#endif // BACKGROUND_H
